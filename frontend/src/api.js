15aab74d2ea7d7607a0d697503883b87193b6439
Afroukh Ines <ines.afroukh.business@gmail.com>
2026-01-03 16:17:23 +0100

    api file updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-export const AUTH = "http://localhost:4001";
-export const RESERVATION = "http://localhost:4002";
-export const NOTIF = "http://localhost:4003";
-export const BILLING = "http://localhost:4004";
+export const AUTH = "http://auth-service.smart-parking.svc.cluster.local:4001";
+export const RESERVATION = "http://reservation-service.smart-parking.svc.cluster.local:4002";
+export const NOTIF = "http://notification-service.smart-parking.svc.cluster.local:4003";
+export const BILLING = "http://billing-service.smart-parking.svc.cluster.local:4004";
